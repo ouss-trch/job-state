6904825753f52afe42ae8f1b46295a348bdfbd70
oussama-trch <oussama.toureche@gmail.com>
2022-01-05 10:27:25 +0100

    job-state-one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Job from './components/Job'
+import Header from './components/header'
+import data from './data.json'
+import { useState } from "react"
+
 
 function App() {
+  // let index = 0
+
+// add state to index of the job and initialise it to 0
+
+  const [index , setIndex] = useState(0)
+
+  //exemple 
+
+  const  moins = ( ) => {
+    if(index > 0 )setIndex(index - 1)
+  }
+
+  const  plus = ( ) => {
+    if(index < data.length - 1)setIndex(index + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {/* <Job /> */}
+      <Job job={data[index]}/>
+      <div>
+        <button 
+        className= "previous" 
+        onClick={()=>{moins()}}> &#8249;</button>
+        <button 
+        className= "next" 
+        onClick={()=>{plus()}}> &#8250;</button>
+      </div>
     </div>
   );
 }
